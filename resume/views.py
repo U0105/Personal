02c32516b5e5f25acf11f715960e36975d777825
U0_105 <hosseinabbasi80.ha@gmail.com>
2024-12-30 08:00:49 -0800

    added new project
    added new project in view and html template

--- resume/views.py
+++ resume/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def resume_view(request):
-
-    return render(request,'resume/resume.html')
+    context = {'FileAnalyzer':'https://github.com/U0105/FileAnalyzer',
+               'MyWebsite':'https://github.com/U0105/Personal',}
+    return render(request,'resume/resume.html',context)
